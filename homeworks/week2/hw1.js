37d6a237062af1b0b605096038b4e0726e96bc41
sixwings <wind.sixwings@gmail.com>
2021-04-21 20:57:09 +0800

    fix-1 修改程式碼 hw6 小修

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,6 +1,31 @@
-function printStars(n){
+/*
+  hw1：印出星星
+  給定 n（1<=n<=30），依照規律「印出」正確圖形
+  printStars(1)
+
+  正確輸出：
+  *
+  printStars(3)
+
+  正確輸出：
+  *
+  *
+  *
+  printStars(6)
+
+  正確輸出：
+  *
+  *
+  *
+  *
+  *
+  *
+  
+*/
+
+function printStars(n) {
   if(Number.isInteger(n)){
-    for(var i=0;i<n;i++){
+    for(var i = 0; i < n; i++){
       console.log('*');
     }
   }else{
