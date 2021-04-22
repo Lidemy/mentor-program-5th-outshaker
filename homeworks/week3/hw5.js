c026e138eb80f45988f36ce6ef75853802a81e3a
sixwings <wind.sixwings@gmail.com>
2021-04-22 20:27:56 +0800

    week3 hw1-6

---
+++ homeworks/week3/hw5.js
@@ -0,0 +1,53 @@
+// hw5 [LIOJ1004 - 聯誼順序比大小](https://oj.lidemy.com/problem/1004)
+
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin
+})
+
+const lines = []
+
+// 讀取到一行，先把這一行加進去 lines 陣列，最後再一起處理
+rl.on('line', (line) => { lines.push(line) })
+
+// 輸入結束，開始針對 lines 做處理
+rl.on('close', () => { solve(lines) })
+
+// x > y: true
+function comp (x, y) {
+  if (x.length > y.length) {
+    return 1
+  } else if (x.length === y.length) {
+    if (x === y) {
+      return 0
+    } else if (x > y) {
+      return 1
+    } else {
+      return -1
+    }
+  } else {
+    return -1
+  }
+}
+
+// 上面都不用管，只需要完成這個 function 就好，可以透過 lines[i] 拿取內容
+function solve (lines) {
+  const m = Number(lines[0])
+  for (let i = 1; i <= m; i++) {
+    const [a, b, k] = lines[i].split(' ')
+    // console.log(a,b,k)
+    const compResult = comp(a, b)
+    if (k === '1' && compResult === 1) {
+      console.log('A')
+    } else if (k === '-1' && compResult === -1) {
+      console.log('A')
+    } else if (k === '1' && compResult === -1) {
+      console.log('B')
+    } else if (k === '-1' && compResult === 1) {
+      console.log('B')
+    } else {
+      console.log('DRAW')
+    }
+  }
+}
