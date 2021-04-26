bc0c94f56adcaacd91971a21a9937ecce1577978
sixwings <wind.sixwings@gmail.com>
2021-04-26 16:27:37 +0800

    week4 add comment to hw

---
+++ homeworks/week4/hw2.js
@@ -0,0 +1,9 @@
+/*
+  hw2：最後的考驗
+  用 node.js 寫出一個程式並接受參數，輸出相對應的結果，範例如下：
+  node hw2.js list // 印出前二十本書的 id 與書名
+  node hw2.js read 1 // 輸出 id 為 1 的書籍
+  node hw2.js delete 1 // 刪除 id 為 1 的書籍
+  node hw2.js create "I love coding" // 新增一本名為 I love coding 的書
+  node hw2.js update 1 "new name" // 更新 id 為 1 的書名為 new name
+*/
---
+++ homeworks/week4/hw3.js
@@ -0,0 +1,26 @@
+/*
+  hw3：周遊列國
+  輸入國家的英文名字，就能夠查詢符合的國家的資訊，會輸出以下幾項：
+  1. 國家名稱
+  2. 首都
+  3. 使用的貨幣名稱
+  4. 電話國碼
+
+  node hw3.js tai
+  ============
+  國家：Taiwan
+  首都：Taipei
+  貨幣：TWD
+  國碼：886
+  ============
+  國家：United Kingdom of Great Britain and Northern Ireland
+  首都：London
+  貨幣：GBP
+  國碼：44
+  ============
+  國家：Lao People's Democratic Republic
+  首都：Vientiane
+  貨幣：LAK
+  國碼：856
+  另外，如果沒有找到任何符合的國家，請輸出：「找不到國家資訊」。
+*/
