6f64bc4371fccc349200b25dcc472bdb2ca0ed3a
sixwings <wind.sixwings@gmail.com>
2021-06-07 11:42:59 +0800

    refactor hw2, a bit RWD

--- homeworks/week8/hw2/js/main.js
+++ homeworks/week8/hw2/js/main.js
@@ -1,27 +1,18 @@
-// ul.games ajax
-// ajax games/top limit offset
-function loadTop5Game() {
-  console.log('loadTop5Game')
+// ajax games/top
+function queryTopGame() {
+  console.log('queryTopGame')
   const req = new XMLHttpRequest()
   req.onload = function(e) {
     if (req.status >= 200 && req.status < 400) {
-      // console.log(req.responseType)
       try {
-        // console.log(req.getAllResponseHeaders())
-        const g = JSON.parse(req.responseText)
-        for (const i in g.top) {
-          games.push(g.top[i].game.name)
-        }
-        console.log(games) // json 版
-        const elems = document.querySelectorAll('.game')
-        for (const i in games) {
-          elems[i].innerText = games[i]
-        }
-        document.querySelector('.games').style.visibility = 'visible'
-        ;[thisGame] = games
-        queryTop20stream()
+        const topGames = JSON.parse(req.responseText)
+        // 載入遊戲列表。這邊說明沒使用 callback 的考量
+        // 目前 callback function 固定一個，所以沒有參數化的需求
+        // 採用 callback 以外的命名方式讓程式比較好讀
+        // callback function 鏈超過兩層就無法從參數列引入，除非使用類似 function call stack 的結構，但那會讓程式更加複雜。
+        loadTopGames(topGames)
       } catch (err) {
-        console.error(err) // 顯示 json parse error
+        console.error(err)
       }
     } else {
       console.error('status: ', req.status) // 顯示 500 錯誤
@@ -32,32 +23,33 @@ function loadTop5Game() {
   req.setRequestHeader('Client-ID', 'it0f8c9ly6exspcpzp4chgtniiyoay')
   req.send()
 }
-// load json to games
-// load games to ul.games
 
-// ajax streams/game limit offset
-// <== clearStreams(), addStream(stream)
-function queryTop20stream() {
-  console.log('top20stream', thisGame)
+// load json and append li.game to ul.games
+function loadTopGames(topGames) {
+  console.log('loadTopGames')
+  const gamesElem = document.querySelector('.games')
+  for (const i in topGames.top) {
+    const liElem = document.createElement('li')
+    liElem.classList.add('game')
+    liElem.dataset.index = i
+    liElem.innerText = topGames.top[i].game.name // gameName
+    gamesElem.appendChild(liElem)
+  }
+  gamesElem.style.visibility = 'visible' // show list after loading
+  thisGameName = topGames.top[0].game.name // 設定起始的遊戲名稱
+  initTopStreams() // 初始化的查詢遊戲熱門實況
+}
+
+// ajax streams/game
+function queryTopStreams(gameName, offset = 0) {
+  console.log('queryTopStreams')
   const req = new XMLHttpRequest()
   req.onload = function(e) {
     if (req.status >= 200 && req.status < 400) {
-      console.log(req.responseType)
       try {
-        clearStreams()
-        // console.log(req.getAllResponseHeaders())
-        streams = JSON.parse(req.responseText).streams
-        for (const i in streams) {
-          console.groupCollapsed() // console.group()
-          console.log(streams[i].preview.medium)
-          console.log(streams[i].channel.status)
-          console.log(streams[i].channel.logo)
-          console.log(streams[i].channel.display_name)
-          console.log(streams[i].channel.name)
-          console.groupEnd()
-          addStream(streams[i])
-          base += streams.length
-        }
+        const topStreams = JSON.parse(req.responseText).streams
+        loadTopStreams(topStreams)
+        checkNoMoreStream(topStreams.length)
       } catch (err) {
         console.error(err) // 顯示 json parse error
       }
@@ -65,71 +57,99 @@ function queryTop20stream() {
       console.error('status: ', req.status) // 顯示 500 錯誤
     }
   }
-  if (base) {
-    req.open('GET', `https://api.twitch.tv/kraken/streams/?game=${encodeURI(thisGame)}&language=zh&limit=20&offset=${base}`)
-  } else {
-    req.open('GET', `https://api.twitch.tv/kraken/streams/?game=${encodeURI(thisGame)}&language=zh&limit=20`)
-  }
+  req.open('GET', `https://api.twitch.tv/kraken/streams/?game=${encodeURI(gameName)}&language=zh&limit=20&offset=${offset}`)
   req.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json')
   req.setRequestHeader('Client-ID', 'it0f8c9ly6exspcpzp4chgtniiyoay')
   req.send()
 }
-// load json to streams
-// append streams to div.streams
 
-// append A stream to div.streams
-function addStream(stream) {
+// load json to streamElem, append streamElem to div.streams
+function loadTopStreams(topStreams) {
+  console.log('loadTopStreams')
   const streamsElem = document.querySelector('.streams')
-  const streamElem = document.createElement('div')
-  streamElem.classList.add('stream')
-  streamElem.innerHTML = `        <div class='stream-cover'><img src='${stream.preview.medium}' alt=''></div>
-        <div class='stream-bottom'>
-          <div class='stream-avater'><img src='${stream.channel.logo}' alt=''></div>
-          <div class='stream-info'>
-            <div class='stream-title' title='${stream.channel.status}'>${stream.channel.status}</div>
-            <div class='stream-host_name'>${stream.channel.display_name}</div>
-          </div>
-        </div>`
-  streamsElem.appendChild(streamElem)
+  console.groupCollapsed('實況列表')
+  for (const i in topStreams) {
+    console.groupCollapsed(topStreams[i].channel.name)
+    console.log(topStreams[i].preview.medium)
+    console.log(topStreams[i].channel.status)
+    console.log(topStreams[i].channel.logo)
+    console.log(topStreams[i].channel.display_name)
+    console.log(topStreams[i].channel.name)
+    console.groupEnd()
+    streamsElem.appendChild(newStreamElem(topStreams[i]))
+  }
+  console.groupEnd()
 }
 
-// clear elements in div.streams
-function clearStreams() {
+// #utility load json, make A div.stream and return it
+function newStreamElem(stream) {
+  const streamElem = document.createElement('div')
+  streamElem.classList.add('stream')
+  streamElem.innerHTML =
+      `<div class='stream-cover'><a href="${stream.channel.url}" target="_blank"><img src='${stream.preview.medium}'></a></div>
+      <div class='stream-bottom'>
+        <div class='stream-avater'><a href="${stream.channel.url}" target="_blank"><img src='${stream.channel.logo}'></a></div>
+        <div class='stream-info'>
+          <div class='stream-title' title='${stream.channel.status}'><a href="${stream.channel.url}" target="_blank">${stream.channel.status}</a></div>
+          <div class='stream-host_name'><a href="${stream.channel.url}" target="_blank">${stream.channel.display_name}</a></div>
+        </div>
+      </div>`
+  return streamElem
+}
+
+// #utility clear all div.streams elements
+function clearAllStreamElems() {
   const streamList = [...document.querySelectorAll('.stream')]
   streamList.forEach((e) => e.remove())
 }
 
-// li.game onclick => data-index => gameName => queryTop20stream(gameName, limit, offset)
-// 本身動作ˋ跟 queryTop20stream() 一樣，差別只是需要帶入 index 參數，可以整理包裝成 loadStreams()
-function queryGame(e) {
-  console.log('queryGame')
-  if (e.target.matches('.game')) {
-    const gameName = games[e.target.dataset.index]
-    console.log('index', e.target.dataset.index)
-    console.log(gameName)
-    thisGame = gameName
-    base = 0
-    queryTop20stream()
+// #utility 檢查是否還有剩餘資料？有的話就顯示「載入更多」按鈕
+function checkNoMoreStream(streamCount) {
+  const btn = document.querySelector('#btn-moreGame')
+  if (streamCount <= 19) {
+    btn.style.display = 'none'
+  } else {
+    btn.style.display = 'block'
+  }
+  if (streamCount === 0) {
+    alert('下面沒有東西囉')
   }
 }
 
-// div.btn-moreGame onclick => thisGame, base => queryTop20stream(gameName, limit, offset)
-// 本身動作ˋ跟 queryTop20stream() 一樣，差別是不需要 clearStreams()
+// 初始頁面查詢熱門實況內容，對 queryTopStreams() 的封裝，非事件觸發
+function initTopStreams() {
+  console.log('initTopStreams')
+  console.log('thisGameName: ', thisGameName)
+  queryTopStreams(thisGameName)
+}
+
+// 其他遊戲選單查詢熱門實況內容，對 queryTopStreams() 的封裝，由 li.game click 事件觸發
+function queryOtherGame(e) {
+  console.log('queryOtherGame')
+  console.log(e.target)
+  console.log(e.target.innerText)
+  if (e.target.matches('.game') && thisGameName !== e.target.innerText) { // 必須是不同的遊戲
+    clearAllStreamElems() // 清空實況列表
+    thisGameName = e.target.innerText // thisGameName 是全域變數，設定目前選取的遊戲名稱
+    queryTopStreams(thisGameName)
+  }
+}
+
+// 「載入更多」 查詢熱門實況內容，對 queryTopStreams() 的封裝，由 div#btn-moreGame onclick 觸發
 function moreGame(e) {
   console.log('moreGame')
-  queryTop20stream()
+  const offset = document.querySelector('.streams').childElementCount // 計算目前載入的實況卡片數量
+  console.log('thisGameName', thisGameName)
+  console.log('offset', offset)
+  queryTopStreams(thisGameName, offset)
 }
 
+// 頁面初始化
 function init() {
-  loadTop5Game()
-  document.querySelector('.games').addEventListener('click', queryGame)
+  document.querySelector('.games').addEventListener('click', queryOtherGame)
   document.querySelector('#btn-moreGame').addEventListener('click', moreGame)
+  queryTopGame()
 }
 
-let games = []
-let thisGame
-
-let streams
-let base = 0
-
+let thisGameName // 紀錄目前選取的遊戲
 document.addEventListener('DOMContentLoaded', init)
