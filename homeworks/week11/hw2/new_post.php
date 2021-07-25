8c04ea63ab34c64cf97ffdd62d8af0cc6b57d259
sixwings <wind.sixwings@gmail.com>
2021-07-25 19:35:14 +0800

    feat: new-post
    新增文章功能

--- homeworks/week11/hw2/new_post.php
+++ homeworks/week11/hw2/new_post.php
@@ -29,7 +29,7 @@
           <li><a href="#">關於我</a></li>
         </div>
         <div>
-          <li><a href="new_post.php">新增文章</a></li>
+          <li><a href="admin.php">管理後台</a></li>
           <li><a href="#">登出</a></li>
         </div>
       </ul>
@@ -43,26 +43,37 @@
   </section>
   <div class="container-wrapper">
     <div class="container">
-      <div class="admin-posts">
-        <div class="admin-post">
-          <div class="admin-post__title">
-              嗨～歡迎來到程式新手村 feat. 胡斯的異想世界
+      <div class="edit-post">
+        <form id="newPostForm" action="cmd_new_post.php" method="POST">
+          <div class="edit-post__title">
+            新增文章：
           </div>
-          <div class="admin-post__info">
-            <div class="admin-post__created-at">
-              2020/07/01 10:15
-            </div>
-            <a class="admin-post__btn" href="edit.php">
-              編輯
-            </a>
-            <a class="admin-post__btn">
-              刪除
-            </a>
+          <div class="edit-post__input-wrapper">
+            <input class="edit-post__input" placeholder="請輸入文章標題" name="title"/>
           </div>
-        </div>
+          <div class="edit-post__input-wrapper">
+            <textarea rows="20" class="edit-post__content" name="content"></textarea>
+          </div>
+          <div class="edit-post__btn-wrapper">
+              <div class="edit-post__btn">送出</div>
+          </div>
+          <input type="hidden" name="status" value="publish"/>
+        </form>
       </div>
     </div>
   </div>
   <footer>Copyright © 2020 Who's Blog All Rights Reserved.</footer>
 </body>
+<script>
+  const btn = document.querySelector('.edit-post__btn');
+  btn.addEventListener('click', () => {
+    const title = document.querySelector(".edit-post__input");
+    const content = document.querySelector(".edit-post__content");
+    if (title.value === '' || content.value === '') {
+      alert('請輸入內容');
+      return;
+    }
+    document.querySelector("#newPostForm").submit();
+  });
+</script>
 </html>
