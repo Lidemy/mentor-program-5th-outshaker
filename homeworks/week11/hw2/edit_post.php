2f11336416e5dbe50ea630b9756b958ed816835a
sixwings <wind.sixwings@gmail.com>
2021-07-25 21:17:05 +0800

    feat: edit post
    編輯文章功能

--- homeworks/week11/hw2/edit_post.php
+++ homeworks/week11/hw2/edit_post.php
@@ -1,3 +1,33 @@
+<?php
+  session_start();
+  require_once('conn.php');
+  require_once('util.php');
+  if (!is_login()) {
+    header("Location: index.php?errCode=403");
+    die();
+  }
+  if(empty($_GET['id'])) {
+    header("Location: admin.php?errCode=400");
+    die();
+  }
+  function get_post($id) {
+    global $conn;
+    $sql = <<<BLOCK
+select * from `sixwings-blog-posts` where `id` = ? limit 1;
+BLOCK;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $id);
+    $isOK = $stmt->execute();
+    if(!$isOK) return false;
+    return $stmt->get_result()->fetch_assoc();
+  }
+  $row = get_post($_GET['id']);
+  if(!$row) {
+    header("Location: admin.php?errCode=404");
+    die();
+  }
+  // var_dump($row);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -35,23 +65,38 @@
   <div class="container-wrapper">
     <div class="container">
       <div class="edit-post">
-        <form action="" method="POST">
+        <form id="editPostForm" action="cmd_edit_post.php" method="POST">
           <div class="edit-post__title">
             發表文章：
           </div>
           <div class="edit-post__input-wrapper">
-            <input class="edit-post__input" placeholder="請輸入文章標題" />
+            <input class="edit-post__input" placeholder="請輸入文章標題" value="<?php echo $row['title']; ?>" name="title"/>
           </div>
           <div class="edit-post__input-wrapper">
-            <textarea rows="20" class="edit-post__content"></textarea>
+            <textarea rows="20" class="edit-post__content" name="content">
+              <?php echo $row['content']; ?>
+            </textarea>
           </div>
           <div class="edit-post__btn-wrapper">
               <div class="edit-post__btn">送出</div>
           </div>
+          <input type="hidden" name="id" value="<?php echo $row['id']; ?>"/>
         </form>
       </div>
     </div>
   </div>
   <footer>Copyright © 2020 Who's Blog All Rights Reserved.</footer>
 </body>
+<script>
+  const btn = document.querySelector('.edit-post__btn');
+  btn.addEventListener('click', () => {
+    const title = document.querySelector(".edit-post__input");
+    const content = document.querySelector(".edit-post__content");
+    if (title.value === '' || content.value === '') {
+      alert('請輸入內容');
+      return;
+    }
+    document.querySelector("#editPostForm").submit();
+  });
+</script>
 </html>
