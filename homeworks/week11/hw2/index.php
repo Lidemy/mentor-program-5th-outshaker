4c3011c6a20e2952d65a0d199b64ca181fb93583
sixwings <wind.sixwings@gmail.com>
2021-07-26 08:26:09 +0800

    feat: index show first 5 posts
    - 首頁顯示前五篇文章功能
    - 相關函式: get_posts(), get_all_posts(), get_page_info()

--- homeworks/week11/hw2/index.php
+++ homeworks/week11/hw2/index.php
@@ -1,3 +1,11 @@
+<?php
+  require_once('util.php');
+  $posts = get_posts(1); // 取得第一頁 (前五篇) 文章
+  if(!$posts) {
+    header("Location: 404.php");
+    die();
+  }
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -34,34 +42,23 @@
   </section>
   <div class="container-wrapper">
     <div class="posts">
+<?php foreach($posts as $post) {?>
       <article class="post">
         <div class="post__header">
-          <div>嗨～歡迎來到程式新手村 feat. 胡斯的異想世界</div>
+          <div><?php echo $post['title']; ?></div>
           <div class="post__actions">
-            <a class="post__action" href="edit.php">編輯</a>
+            <!--<a class="post__action" href="edit.php">編輯</a>-->
           </div>
         </div>
         <div class="post__info">
-          2020/07/01 11:11:12
+          <?php echo $post['posted_at']; ?>
         </div>
-        <div class="post__content">郭沫若說過一句富有哲理的話，形成天才的決定因素應該是勤奮。這句話語雖然很短，但令我浮想聯翩。每個人都不得不面對這些問題。在面對這種問題時， 我認為， 生活中，若中午吃什麼出現了，我們就不得不考慮它出現了的事實。中午吃什麼，發生了會如何，不發生又會如何。我們一般認為，抓住了問題的關鍵，其他一切則會迎刃而解。我們都知道，只要有意義，那麼就必須慎重考慮。現在，解決中午吃什麼的問題，是非常非常重要的。所以， 那麼， 一般來講，我們都必須務必慎重的考慮考慮
+        <div class="post__content">
+          <?php echo $post['content']; ?>
         </div>
-        <a class="btn-read-more" href="blog.php">READ MORE</a>
+        <a class="btn-read-more" href="post.php?id=<?php echo $post['id']; ?>">READ MORE</a>
       </article>
-      <article class="post">
-        <div class="post__header">
-          <div>嗨～歡迎來到程式新手村 feat. 胡斯的異想世界</div>
-          <div class="post__actions">
-            <a class="post__action" href="#">編輯</a>
-          </div>
-        </div>
-        <div class="post__info">
-          2020/07/01 11:11:12
-        </div>
-        <div class="post__content">郭沫若說過一句富有哲理的話，形成天才的決定因素應該是勤奮。這句話語雖然很短，但令我浮想聯翩。每個人都不得不面對這些問題。在面對這種問題時， 我認為， 生活中，若中午吃什麼出現了，我們就不得不考慮它出現了的事實。中午吃什麼，發生了會如何，不發生又會如何。我們一般認為，抓住了問題的關鍵，其他一切則會迎刃而解。我們都知道，只要有意義，那麼就必須慎重考慮。現在，解決中午吃什麼的問題，是非常非常重要的。所以， 那麼， 一般來講，我們都必須務必慎重的考慮考慮
-        </div>
-        <a class="btn-read-more">READ MORE</a>
-      </article>
+<?php }?>
     </div>
   </div>
   <footer>Copyright © 2020 Who's Blog All Rights Reserved.</footer>
--- homeworks/week11/hw2/util.php
+++ homeworks/week11/hw2/util.php
@@ -1,6 +1,86 @@
 <?php
+  require_once('conn.php');
+
   function is_login() {
     return session_status() === PHP_SESSION_ACTIVE &&
            isset($_SESSION) && !empty($_SESSION['is_login']);
+  }
+  
+  // 抓取文章，因為多頁面共用所以移到這邊
+  function get_posts($page=0, $page_size=5) {
+    if ($page === 0) return get_all_posts();
+    global $conn;
+    $num_pages = intval(get_page_info()['num_pages']);
+    $page = ($page > $num_pages) ? $num_pages : (($page < 0) ? 1 : $page);
+    $limit = $page_size;
+    $offset = ($page - 1) * $page_size;
+    $sql = <<<BLOCK
+  SELECT *
+  FROM `sixwings-blog-posts`
+  WHERE status != 'deleted'
+  ORDER BY id DESC
+  LIMIT ? OFFSET ?;
+BLOCK;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('ii', $limit, $offset);
+    $isOK = $stmt->execute();
+    if (!$isOK) {
+      error_log("{$stmt->errno}: {$stmt->error}\n", 3, "debug.log");
+      return false;
+    }
+    $result = $stmt->get_result();
+    if (!$result) return false;
+    $arr = array();
+    while($row = $result->fetch_assoc()) {
+      $arr[] = $row;
+    }
+    return $arr;
+  }
+
+  // 取得所有文章
+  function get_all_posts() {
+    global $conn;
+    $sql = <<<BLOCK
+  SELECT *
+  FROM `sixwings-blog-posts`
+  WHERE status != 'deleted'
+  ORDER BY id DESC
+BLOCK;
+    $stmt = $conn->prepare($sql);
+    $isOK = $stmt->execute();
+    if (!$isOK) {
+      error_log("{$stmt->errno}: {$stmt->error}\n", 3, "debug.log");
+      return false;
+    }
+    $result = $stmt->get_result();
+    if (!$result) return false;
+    $arr = array();
+    while($row = $result->fetch_assoc()) {
+      $arr[] = $row;
+    }
+    return $arr;
+  }
+
+  // 分頁相關 page_size 一頁資料量, count 資料數量, num_pages 資料頁數
+  function get_page_info($page_size = 5) {
+    global $conn;
+    $sql = <<<BLOCK
+SELECT
+COUNT(id) AS count,
+CEIL(COUNT(id) / {$page_size}) AS num_pages
+FROM `sixwings-blog-posts` WHERE status != 'deleted';
+BLOCK;
+    $stmt = $conn->prepare($sql);
+    $isOK = $stmt->execute();
+    if (!$isOK) {
+      error_log("{$stmt->errno}: {$stmt->error}\n", 3, "debug.log");
+      return false;
+    }
+    return $stmt->get_result()->fetch_assoc();
+  }
+
+  // 轉義文字: 防止 XSS
+  function escape($str) {
+    return htmlspecialchars($str, ENT_QUOTES);
   }
 ?>
